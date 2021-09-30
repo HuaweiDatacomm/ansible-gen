ba47523da6490607fb174991c69c210a001ccfc6
lllyfeng <frank.fengchong@huawei.com>
2021-09-30 11:33:36 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ansible-gen',
+    version='0.0.1beta',
+    packages=['ansible_gen', 'ansible_gen.adapter', 'ansible_gen.adapter.utils', 'ansible_gen.adapter.utils.xml_parse',
+              'ansible_gen.adapter.utils.yang_parse', 'ansible_gen.generator'],
+    url='https://github.com/HuaweiDatacomm/ansible-gen',
+    license='Apache License, Version 2.0',
+    author='frank feng',
+    author_email='frank.fengchong@huawei.com',
+    description='a tool can generate ansible api according yang modules and some input xml files'
+)
