51e29f26283ebfe3773d9a157e049d875490f1ac
niubiwocao <93188931+niubiwocao@users.noreply.github.com>
2022-01-10 15:25:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ansible-gen',    version='0.6.1',
+    name='ansible-gen',    version='0.7.0',
     packages=find_packages(),
     include_package_data=True,
     entry_points={
